fb6d209e5defa6f3d4dff96eb2ca92d70f2bf4ac
Karim <crazychixl0@gmail.com>
2025-06-22 18:02:33 +0600

    Добавьте файлы проекта.

---
+++ SmartO!rder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartO_rder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
